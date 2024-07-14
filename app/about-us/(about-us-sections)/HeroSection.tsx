d4c4efb755305c4eab460e82d50760057c3e21fc
76marin76 <76marin76@gmail.com>
2024-07-14 23:56:13 +0200

    hero section of about route restyled

--- app/about-us/(about-us-sections)/HeroSection.tsx
+++ app/about-us/(about-us-sections)/HeroSection.tsx
@@ -1,32 +1,23 @@
 import React from "react";
-import SectionWrapper from "../../components/SectionWrapper";
 import '../../../public/test.css'
 import Image from "next/image";
-import HeroImage from '../../../public/images/about-us/IgorImg.svg'
-import HeroImageTwo from '../../../public/images/about-us/MarinImg.svg'
-
+import HeroImage from '../../../public/images/home/undraw_collaborators_re_hont 1.svg'
+import ArrowRight from '../../../public/images/Rectangle 1.svg'
+import HeroSectionWrapper from "../../components/HeroSectionWrapper";
 
 export default function HeroSection() {
     return (
-        <SectionWrapper>
-            <p className="text-lg font-bold text-black uppercase">Get to know us</p>
-            <h1 className="text-5xl text-primary-brown font-semibold">
-                About Us
+        <HeroSectionWrapper>
+            <p className="text-xs md:text-md font-bold md:font-bold text-[#ffffff] uppercase pt-2">Who are we?</p>
+            <h1 className="text-5xl text-[#ffffff] font-semibold text-start">
+                We are a team of designers and developers who brings you 
+                <span className="custom-text font-black">
+                    {" "}best results {" "}
+                </span>
             </h1>
-            {/* <div className="w-full flex">
-                <button className="flex items-center gap-2 py-2 px-6 text-[#ffffff] bg-gradient-to-r from-[#1A9FFF] to-[#0083E0]  rounded-full text-xl">
-                    <Image src={ArrowRight} alt="hero_image"  />
-                    Get in touch
-                </button>
-            </div> */}
-            <div className="w-full flex flex-col gap-4 bg-red-400">
-                <div className="flex items-center justify-start">
-                    <Image src={HeroImage} alt="hero_image" className="text-end" />
-                </div>
-                <div className="flex items-center justify-end">
-                    <Image src={HeroImageTwo} alt="hero_image" className="rotate-180" />
-                </div>
+            <div className="bg-red-400 w-full h-full">
+                <Image src={HeroImage} alt="hero_image" className="" />
             </div>
-        </SectionWrapper>
+        </HeroSectionWrapper>
     )
 }
