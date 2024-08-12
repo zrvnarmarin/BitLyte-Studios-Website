f4973a3c0387201db66f090cc1cdedb2e30d4c6a
zrvnarmarin <76marin76@gmail.com>
2024-08-12 18:47:22 +0200

    tetimonial section restyling;

--- app/(home-sections)/TestimonialsSection.tsx
+++ app/(home-sections)/TestimonialsSection.tsx
@@ -11,18 +11,32 @@ import { LeftArrow, RightArrow } from "./ServicesSection";
 export default function TestimonialsSection() {
   return (
     <SectionWrapper>
-      <h2 className="w-full text-5xl text-[#ffffff] z-20 pb-12">
-        What our clients say
-      </h2>
       <div className="flex items-center justify-between">
-        <button className="relative overflow-hidden rounded-full p-4 border-[0.5px] border-[#252525]">
-          {/* RightArrow with a higher z-index */}
-          <div className="relative z-20">
-            <LeftArrow />
-          </div>
-          {/* Blur background */}
-          <div className="w-full h-full absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#002139] z-10 blur-[20px]"></div>
-        </button>
+        <h2 className="w-full text-5xl text-[#ffffff] z-20 pb-12">
+          What our clients say
+        </h2>
+
+        <div className="flex items-center justify-center gap-4">
+          <button className="relative overflow-hidden rounded-full p-4 border-[0.5px] border-[#252525]">
+            {/* RightArrow with a higher z-index */}
+            <div className="relative z-20">
+              <LeftArrow />
+            </div>
+            {/* Blur background */}
+            <div className="w-full h-full absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#002139] z-10 blur-[20px]"></div>
+          </button>
+          <button className="relative overflow-hidden rounded-full p-4 border-[0.5px] border-[#252525]">
+            {/* RightArrow with a higher z-index */}
+            <div className="relative z-20">
+              <RightArrow />
+            </div>
+            {/* Blur background */}
+            <div className="w-full h-full absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#002139] z-10 blur-[20px]"></div>
+          </button>
+        </div>
+
+      </div>
+      <div className="flex items-center justify-between">
 
         {/* Testimonial cards container */}
         <div className="relative w-full flex flex-col justify-center items-center gap-8 pt-8">
@@ -39,26 +53,17 @@ export default function TestimonialsSection() {
           ))}
         </div>
 
-        <button className="relative overflow-hidden rounded-full p-4 border-[0.5px] border-[#252525]">
-          {/* RightArrow with a higher z-index */}
-          <div className="relative z-20">
-            <RightArrow />
-          </div>
-          {/* Blur background */}
-          <div className="w-full h-full absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#002139] z-10 blur-[20px]"></div>
-        </button>
       </div>
 
       {/* Buttons */}
-      <div className="w-flex flex items-center justify-center">
+      {/* <div className="w-flex flex items-center justify-center">
         <div className="relative overflow-hidden w-fit flex items-center justify-center gap-2 rounded-full px-6 py-2 bg-[#252525]">
-          {/* Blur background */}
           <div className="w-96 h-96 absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#0083E0] z-10 blur-[130px]"></div>
           <div className="h-2 w-2 rounded-full bg-[#ffffff] z-20"></div>
           <div className="h-2 w-2 rounded-full bg-[#ffffff] z-20"></div>
           <div className="h-2 w-2 rounded-full bg-[#ffffff] z-20"></div>
         </div>
-      </div>
+      </div> */}
     </SectionWrapper>
   );
 }
@@ -71,22 +76,25 @@ export const TestimonialCard = ({
   rating,
 }: TestimonialCardProps) => {
   return (
-    <div className="relative overflow-hidden rounded-lg flex flex-row items-center border-[0.5px] border-[#252525] p-4 w-2/3">
+    <div className="relative overflow-hidden rounded-lg flex flex-row items-center  border-[0.5px] border-[#252525] px-24 py-24">
       {/* Blur background */}
       <div className="w-full h-full absolute right-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#002139] z-10 blur-[130px]"></div>
-
+      
       {/* Testimonial */}
       <div className="flex flex-col z-20">
-        <p className="text-base text-[#eeeeee] font-normal text-start py-4 pb-6 px-8 z-20">
+        <div className="w-full flex items-center justify-center pb-6">
+          <QuotesIcon />
+        </div>
+        <p className="w-full text-4xl text-[#ffffff] font-light leading-snug text-center z-20">
           {testimonialText}
         </p>
-        <div className="flex items-center justify-start gap-2 pt-2 px-8">
+        <div className="flex items-center justify-center gap-2 pt-12">
           <Image src={imageSrc} alt="testimonial image" />
-          <p className="flex flex-col items-center">
-            <span className="w-full text-xl text-[#eeeeee] font-bold">
+          <p className="flex flex-col items-center gap-1">
+            <span className="w-full text-2xl text-[#ffffff] font-semibold">
               {name}
             </span>
-            <span className="w-full text-base text-[#eeeeee] font-normal ">
+            <span className="w-full text-lg text-[#eeeeee] font-normal ">
               {companyName}
             </span>
           </p>
@@ -111,7 +119,7 @@ export const testimonials = [
     name: "Nick Forest",
     companyName: "CEO Of WordPress",
     testimonialText:
-      "One of the standout features of [Web Service Name] is its intuitive and user-friendly interface. The platform is designed with the end user in mind, making it incredibly easy to navigate even for someone with minimal technical skills. The setup process was straightforward, and the step-by-step guides provided were both comprehensive and easy to follow. Within no time, I was able to customize my site and integrate various features tailored to my business needs.",
+      "One of the standout features of web development is its intuitive and user-friendly interface. The platform is designed with the end user in mind, making it incredibly easy to navigate even for someone with minimal technical skills. The setup process was straightforward, and the step-by-step guides provided were both comprehensive and easy to follow. ",
     imageSrc: TestimonialExampleImage,
     rating: 5,
   },
@@ -124,3 +132,22 @@ export const testimonials = [
   //     rating: 5
   // }
 ];
+
+export const QuotesIcon = ({ width = 54, height = 54 }) => (
+  <svg
+    fill="#ffffff"
+    width={width}
+    height={height}
+    viewBox="0 0 123.961 123.961"
+    style={{ transform: 'scale(0.7)', transformOrigin: 'center' }}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+    <g id="SVGRepo_iconCarrier">
+      <g>
+        <path d="M49.8,29.032c3.1-1.3,4.4-5,3-8l-4.9-10.3c-1.4-2.899-4.8-4.2-7.8-2.899c-8.5,3.6-15.8,8.3-21.6,14 C11.4,28.532,6.6,36.232,4,44.732c-2.6,8.601-4,20.3-4,35.2v30.7c0,3.3,2.7,6,6,6h39.3c3.3,0,6-2.7,6-6v-39.3c0-3.301-2.7-6-6-6 H26.5c0.2-10.101,2.6-18.2,7-24.301C37.1,36.133,42.5,32.133,49.8,29.032z" />
+        <path d="M120.4,29.032c3.1-1.3,4.399-5,3-8l-4.9-10.199c-1.4-2.9-4.8-4.2-7.8-2.9c-8.4,3.6-15.601,8.3-21.5,13.9 c-7.101,6.8-12,14.5-14.601,23c-2.6,8.399-3.899,20.1-3.899,35.1v30.7c0,3.3,2.7,6,6,6H116c3.3,0,6-2.7,6-6v-39.3 c0-3.301-2.7-6-6-6H97.1c0.2-10.101,2.601-18.2,7-24.301C107.7,36.133,113.1,32.133,120.4,29.032z" />
+      </g>
+    </g>
+  </svg>
+);
