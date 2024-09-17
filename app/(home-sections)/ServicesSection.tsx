e12dd82378a852d14b81fe15c9be1cfe5fc227df
76marin76 <76marin76@gmail.com>
2024-09-17 13:31:39 +0200

    restyling services section on home page

--- app/(home-sections)/ServicesSection.tsx
+++ app/(home-sections)/ServicesSection.tsx
@@ -32,7 +32,7 @@ export const ServiceCategoryCard = ({
   services,
 }: ServiceCardType) => {
   return (
-    <div className="group relative overflow-hidden w-full grid grid-cols-2 justify-between gap-4 rounded-lg border-[0.5px] border-[#252525] z-20">
+    <div className="group relative overflow-hidden w-full grid grid-cols-2 justify-between gap-4 rounded-lg  z-20">
       {/* Vidjdti da li ovo ostaviti ili izbrisati */}
       <div className="w-48 h-48 absolute left-0 bg-gradient-to-r from-[#1A9FFF] to-[#0086E6] z-10 blur-[230px] pointer-events-none"></div>
 
@@ -56,7 +56,7 @@ export const ServiceCategoryCard = ({
           <Link
             href={`/services/${service}`}
             key={service}
-            className="group h-full w-full flex  gap-6 relative overflow-hidden border-[0.5px] border-l-[#252525] bg-gradient-to-b from-[#0b0b0b] to-[#27a5ff2b]"
+            className="group h-full w-full flex  gap-6 relative overflow-hidden border-[0.5px] border-l-[#252525] border-y-[#252525] bg-gradient-to-b from-[#0b0b0b] to-[#27a5ff2b]"
           >
             {/* Blur background */}
             <div className="w-24 h-24 absolute left-0 bottom-0 bg-gradient-to-r from-[#1A9FFF] to-[#0086E6] z-10 blur-[230px]"></div>
