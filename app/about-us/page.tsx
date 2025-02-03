f571ee519b548f65ba673fac7fae54abb206ac21
zrvnarmarin <76marin76@gmail.com>
2025-02-03 23:41:11 +0100

    trying to resolve the caching problem;

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <MissionAndVision />
       <WhatWeStandForSection /> 
       <ValuesSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <CTASection />
     </main>
   );
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <HeroSection />
       <ServiceCategoriesSection />
       <OurProcessessSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <CTASection />
     </main>
   );
