992bdeb0bf1fb874813b26824af5dd64b693cf1f
76marin76 <76marin76@gmail.com>
2025-03-04 03:10:19 +0100

    gradient fix;

--- app/(home-sections)/ProjectsSection.tsx
+++ app/(home-sections)/ProjectsSection.tsx
@@ -5,7 +5,10 @@ import SectionWrapper from "../components/SectionWrapper";
 import Link from "next/link";
 import TestImage from "../../public/apartment.webp";
 import { LogoForButtonRotated } from "../services/[serviceId]/HeroSection";
-import { projectsThumbnailData, ProjectThumbnailDataProps } from "../projects/projectsThumbnailData";
+import {
+  projectsThumbnailData,
+  ProjectThumbnailDataProps,
+} from "../projects/projectsThumbnailData";
 // import { ProjectCard } from "../projects/ProjectsListSection";
 
 export default function ProjectsSection() {
@@ -157,8 +160,8 @@ export const ProjectCard = ({
       className="relative overflow-hidden flex flex-col rounded-lg border-[0.5px] border-[#0a2030] hover:shadow-[0_0_15px_0_rgba(10,32,78,1)]"
     >
       {/* Blur gradients */}
-      <div className="w-96 h-96 absolute top-0 bottom-0 bg-gradient-to-r from-[#1a4869] to-[#002139] z-10 blur-[230px]"></div>
-        <div className="w-96 h-96 absolute left-0 right-0 bg-gradient-to-r from-[#1a4869] to-[#002139] z-10 blur-[230px]"></div>
+      <div className="w-48 h-48 lg:w-96 lg:h-96 absolute top-0 bottom-0 bg-gradient-to-r from-[#1a4869] to-[#002139] z-10 blur-[230px]"></div>
+      <div className="w-96 h-96 absolute left-0 right-0 bg-gradient-to-r from-[#1a4869] to-[#002139] z-10 blur-[230px]"></div>
       {/* Project image */}
       <div className="relative w-full h-[300px] sm:h-[400px] lg:h-[400px] flex items-center z-20 overflow-hidden">
         <Image
