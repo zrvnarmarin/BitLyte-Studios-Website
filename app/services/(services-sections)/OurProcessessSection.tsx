14467cd167d726ebcc5e8cc87fa1d80d6cd887c8
76marin76 <76marin76@gmail.com>
2025-03-07 02:04:04 +0100

    processesSection restyled;

--- app/services/(services-sections)/OurProcessessSection.tsx
+++ app/services/(services-sections)/OurProcessessSection.tsx
@@ -17,6 +17,7 @@ export default function OurProcessessSection() {
             id={step.id}
             title={step.title}
             description={step.description}
+            steps={step.steps}
           />
         ))}
       </div>
@@ -28,25 +29,37 @@ export const ProcessStepCard = ({
   id,
   title,
   description,
+  steps,
 }: ProcessessStepType) => {
   return (
     <div
       key={id}
-      className={`relative overflow-hidden flex-1 px-6 py-10 rounded-lg border-[0.5px] border-[#0a2030]`}
+      className={`relative overflow-hidden flex flex-col px-6 py-6 rounded-lg border-[0.5px] border-[#0a2030]`}
     >
       {/* Blur background */}
       <div className="w-32 h-32 absolute bottom-0 top-0 bg-gradient-to-r from-[#1A9FFF] to-[#0086E6] z-10 blur-[130px] pointer-events-none"></div>
 
-      <div className="w-full flex flex-col xs:flex-row items-center justify-start lg:justify-center z-20">
-        <span className="custom-text font-black text-7xl z-0 mr-4">{id}</span>
-        <span className="text-[#ffffff] font-semibold text-3xl text-center md:text-start x z-20">
+      <div className="w-full flex flex-col items-start justify-start lg:justify-start gap-2 z-20">
+        <span className="custom-text font-black text-4xl z-20">{id}</span>
+        <span className="text-[#ffffff] font-semibold text-3xl text-start md:text-start x z-20">
           {title}
         </span>
       </div>
 
-      <p className="text-xl font-light text-[#ffffff] text-center xs:text-start lg:text-center xl:text-start pt-6 z-20">
+      <p className="text-xl font-light text-[#ffffff] text-start py-6 z-20">
         {description}
       </p>
+
+      <div className="mt-auto pt-6 border-t border-[#1A3050]/50">
+        <ul className="flex flex-col gap-2">
+          {steps.map((feature, i) => (
+            <li key={i} className="flex items-start gap-2">
+              <CircleCheckBigIcon />
+              <span className="text-sm text-[#ffffff]">{feature}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
@@ -54,23 +67,27 @@ export const ProcessStepCard = ({
 const processesSteps: ProcessessStepType[] = [
   {
     id: 1,
-    title: "Discovery & Strategy",
-    description: `We start by learning about your goals and target audience through meetings and research to shape the right approach.`,
+    title: "Discovery & Talking",
+    description: `We start by learning about your meetings and research to shape the right approach.`,
+    steps: ["Frontend development", "Backend integration", "Quality assurance"],
   },
   {
     id: 2,
-    title: "Design & Planning",
-    description: `Using our insights, we create tailored strategies and designs that showcase your brand and effectively engage your audience.`,
+    title: "Design & Development",
+    description: `Using our insights, showcase brand and engage your audience.`,
+    steps: ["Frontend development", "Backend integration", "Quality assurance"],
   },
   {
     id: 3,
-    title: "Development & Testing",
-    description: `We bring the plan to life by developing high-quality, functional digital solutions that align with both user and business needs.`,
+    title: "Testing & Answering",
+    description: `We bring the plan to high-quality, functional user and business needs.`,
+    steps: ["Frontend development", "Backend integration", "Quality assurance"],
   },
   {
     id: 4,
-    title: "Delivery & Launching",
-    description: `After launching, we provide continuous support and track results, refining where necessary to ensure long-term success.`,
+    title: "Launching & Learning",
+    description: `After launching, we provide and track results long-term success.`,
+    steps: ["Frontend development", "Backend integration", "Quality assurance"],
   },
 ];
 
@@ -78,4 +95,24 @@ export type ProcessessStepType = {
   id: number;
   title: string;
   description: string;
+  steps: string[];
 };
+
+const CircleCheckBigIcon = ({
+  width = 18,
+  height = 18,
+  color = "currentColor",
+}) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    stroke={`#0c90ef`}
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+    <path d="m9 11 3 3L22 4"></path>
+  </svg>
+);
