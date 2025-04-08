e5504bca43f5585cc05446479931ed36e816c22d
76marin76 <76marin76@gmail.com>
2025-04-08 12:34:30 +0200

    changed padding on why us section cards on home page;

--- app/(home-sections)/WhyUsSection.tsx
+++ app/(home-sections)/WhyUsSection.tsx
@@ -44,7 +44,7 @@ export const WhyUsCard = ({ index, title, description, icon }: WhyUsCardType) =>
         <div className="flex items-center z-30">
           {React.createElement(icon)}
         </div>
-        <h3 className="w-full text-[#ffffff] font-semibold text-3xl sm:text-[28px] md:text-3xl leading-tight text-start sm:text-center z-20">
+        <h3 className="w-full text-[#ffffff] font-semibold text-3xl sm:text-[28px] md:text-3xl pt-2 leading-tight text-start sm:text-center z-20">
           {title}
         </h3>
         <p className="text-xl font-light text-[#ffffff] text-start sm:text-center pt-4 sm:pt-6 z-40">
